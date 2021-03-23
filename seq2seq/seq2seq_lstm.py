fe015e6afe1b112ed2b4f3139ebb496586030d51
Raj Nath Patel <patelrajnath@gmail.com>
2021-03-23 19:54:19 +0000

    Add code for seq2seq tutorial

---
+++ seq2seq/seq2seq_lstm.py
@@ -0,0 +1,86 @@
+import spacy
+import torch
+from torch import nn
+from torchtext.legacy import data
+from torchtext.legacy.data import Field
+from torchtext.legacy.datasets import Multi30k
+
+spacy_de = spacy.load('de_core_news_sm')
+spacy_en = spacy.load('en_core_web_sm')
+
+
+def tokenize_de(text):
+    return [tok.text for tok in spacy_de.tokenizer(text)]
+
+
+def tokenize_en(text):
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+
+
+SRC = Field(tokenize=tokenize_de, init_token='<sos>', eos_token='<eos>', lower=True)
+TRG = Field(tokenize=tokenize_de, init_token='<sos>', eos_token='<eos>', lower=True)
+
+train_data, valid_data, test_data = Multi30k.splits(exts=('.de', '.en'), fields=(SRC, TRG))
+
+MIN_FREQ = 2
+SRC.build_vocab(train_data.src, min_freq=MIN_FREQ)
+TRG.build_vocab(train_data.trg, min_freq=MIN_FREQ)
+
+train_iter = data.Iterator(train_data, batch_size=1,
+                           device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+                           repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
+                           train=True)
+
+input_dim = len(SRC.vocab.stoi)
+output_dim = len(TRG.vocab.stoi)
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, emb_dim, hid_dim, num_layers, dropout=0.01):
+        super(Encoder, self).__init__()
+        self.input_dim = input_dim
+        self.emb_dim = emb_dim
+        self.hid_dim = hid_dim
+        self.num_layers = num_layers
+        self.dropout = dropout
+
+        self.embeddings = nn.Embedding(self.input_dim, self.emb_dim)
+        self.rnn = nn.LSTM(self.emb_dim, self.hid_dim, self.num_layers)
+        self.dropout_layer = nn.Dropout(self.dropout)
+
+    def forward(self, x):
+        tensor = self.embeddings(x)
+        tensor = self.dropout_layer(tensor)
+
+        outputs, (hidden, cell) = self.rnn(tensor)
+
+        # outputs = [sen_len, batch_size, hid_dim * n_directions]
+        # hidden = [n_layers * n_direction, batch_size, hid_dim]
+        # cell = [n_layers * n_direction, batch_size, hid_dim]
+
+        return hidden, cell
+
+
+class Decoder(nn.Module):
+    def __init__(self, output_dim, emb_dim, hid_dim, num_layers, dropouts=0.01):
+        super(Decoder, self).__init__()
+        self.output_dim = output_dim
+        self.emb_dim = emb_dim
+        self.hid_dm = hid_dim
+        self.num_layers = num_layers
+        self.dropout = dropouts
+
+        self.embeddings = nn.Embedding(output_dim, emb_dim)
+        self.rnn = nn.LSTM(emb_dim, hid_dim, num_layers)
+        self.translate = nn.Linear(hid_dim, output_dim)
+
+    def foward(self, y, hidden):
+        pass
+
+
+encoder = Encoder(input_dim, 16, 16, 1)
+
+for batch in train_iter:
+    print(batch.src.size())
+    encoder(batch.src)
+    exit()
